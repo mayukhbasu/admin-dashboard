bcdfacbc9d3dbcccd3bf7a541e36518df6c0de72
Bubul <bubul@Bubuls-MBP.Dlink>
2022-09-06 12:19:00 +0530

    basic setup completed

---
+++ src/pages/ECommerce.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ECommerce = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ECommerce;
---
+++ src/pages/Editors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Editors = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Editors;
