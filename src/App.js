d76a244fdc8f9a998684dab5bb558d10f0b0de69
Bubul <bubul@Bubuls-MBP.Dlink>
2022-09-07 20:41:48 +0530

    finished ecom page

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
             <Navbar/>
           </div>
-        </div>
+        
         <div>
           <Routes>
             <Route path='/' element={<Ecommerce/>}/>
@@ -57,6 +57,7 @@ function App() {
             <Route path='/pyramid' element={<Pyramid/>}/>
             <Route path='/stacked' element={<Stacked/>}/>
           </Routes>
+        </div>
         </div>
        </div>
       </BrowserRouter>
