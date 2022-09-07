035ed629c26f6ce0eadcf816c322d7f7573e4591
Bubul <bubul@Bubuls-MBP.Dlink>
2022-09-07 20:17:22 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { BsChatLeft } from 'react-icons/bs';
 import { FiShoppingCart } from 'react-icons/fi';
@@ -25,9 +25,22 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 )
 const Navbar = () => {
     const {currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setIsClicked, setScreenSize, screenSize } = useStateContext();
+    
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if(screenSize <= 900) setActiveMenu(false);
+        else {
+            setActiveMenu(true);
+        }
+    }, [screenSize])
+
     return (
-       
-       
         <div className='flex justify-between p-2 md:mx-6 relative'>
             <NavButton title="Menu" 
             customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue"
